3706267b805e7631b6b7ec1cb78397d33fe7e9c6
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-06-14 21:58:20 -0500

    task 0 and fix sort.h

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+/**
+ * void bubble_sort - order with Bubble sort.
+ *@array: expected to a sort.
+ *@size: size of the a array.
+ * Return: void.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, more_value;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				more_value = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = more_value;
+				print_array(array, size);
+			}
+		}
+	}
+}
